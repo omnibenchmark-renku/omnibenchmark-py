e6f693b1ec5d6e46eff027a14c9295c620846fe8
almutlue <almut.lue@gmail.com>
2022-07-05 08:30:12 +0200

    version bumb 0.0.1 --> 0.0.2

--- omnibenchmark/__init__.py
+++ omnibenchmark/__init__.py
@@ -1,5 +1,8 @@
 """Omnibenchmark"""
 
+# Version of the omnibenchmark package
+__version__ = "0.2.0"
+
 from omnibenchmark import core, management, renku_commands, utils
 
 __all__ = ("core", "management", "renku_commands", "utils")
